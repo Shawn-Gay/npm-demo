dd8bb5a69853d1a80b094d2b34bd2d163e4519ca
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2025-09-13 22:31:43 -0500

    npm setup for components

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// Components
+export { Button, buttonVariants } from "./components/button"
+export { Input } from "./components/input"
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent
+} from "./components/card"
+
+// Utils
+export { cn } from "./lib/utils"
+
+// Types
+export type { ButtonProps } from "./components/button"
+export type { InputProps } from "./components/input"
